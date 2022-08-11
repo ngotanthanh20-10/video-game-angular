6231977bdc54f75cb227373d62acb4499b34de82
ngotanthanh20-10 <thanhngoangiang@gmail.com>
2022-08-12 00:44:29 +0700

    456

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { APIResponse, Game } from 'src/app/models';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  // public sort: string;
+  // public games: Array<Game>;
+  // private routeSub: Subscription;
+  // private gameSub: Subscription;
+  
   constructor() { }
 
   ngOnInit(): void {
